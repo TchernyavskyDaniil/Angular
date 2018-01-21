a7b980298f51bf9ffc81171e0c43a869f761da6a
TchernyavskyDaniil <tchernyavsky.daniil@yandex.ru>
2018-01-21 03:16:30 +0300

    complited map api

--- src/assets/js/map.js
+++ src/assets/js/map.js
@@ -1,17 +1,15 @@
 function init() {
-    const block = document.querySelector('.main__placeholder');
     const blockApi = document.querySelector('.pop-up');
     myMap=new ymaps.Map("map", {
-            center: [45.059424, 39.0059951],
+            center: [45.043124, 39.0056951],
             zoom: 13,
             controls: []
         }
     ),
-      myPlacemark=new ymaps.Placemark([45.043124, 39.0056951], {
-          hintContent: "sometext in hover",
+      myPlacemark=new ymaps.Placemark([45.043124, 39.0056951],
+        {
           balloonContent: "Это наш офис"
-        }
-        ,
+        },
         {
           iconLayout: "default#image",
           // iconImageHref: "../img/placeholder.",
@@ -20,13 +18,13 @@ function init() {
         }
         )
     myMap.geoObjects.add(myPlacemark);
-    ymaps.domEvent.manager
-      .add(block, 'mouseenter', function (event) {
+    myPlacemark.events
+      .add('mouseenter', function (event) {
         event.preventDefault();
         console.log("one");
         blockApi.classList.toggle('pop-up-close');
       })
-      .add(block, 'mouseleave', function (event) {
+      .add('mouseleave', function (event) {
         event.preventDefault();
         blockApi.classList.toggle('pop-up-close');
       })
